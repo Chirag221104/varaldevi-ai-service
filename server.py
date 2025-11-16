7b60f5f5c860f1e0c58bc73cd4c4ca6e3b3af1ce
Chirag Malde <m.chirag2211@gmail.com>
2025-11-17 04:56:00 +0530

    Remove TensorFlow, add dummy AI model

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
 import numpy as np
 from PIL import Image
 import requests
@@ -7,12 +6,7 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-# --- TEMPORARY DUMMY MODEL (works without training) ---
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(224, 224, 3)),
-    tf.keras.layers.Dense(10, activation="softmax")
-])
-
+# Dummy labels
 labels = [
     "garbage",
     "plastic_bottles",
@@ -28,9 +22,9 @@ labels = [
 
 def preprocess(img):
     img = img.resize((224, 224))
-    arr = tf.keras.preprocessing.image.img_to_array(img)
-    arr = tf.expand_dims(arr, 0)
-    return arr / 255.0
+    arr = np.array(img)
+    arr = arr.reshape(1, -1)  # flatten
+    return arr
 
 @app.post("/")
 def analyze():
@@ -40,14 +34,15 @@ def analyze():
     try:
         response = requests.get(image_url)
         img = Image.open(BytesIO(response.content)).convert("RGB")
-        arr = preprocess(img)
 
-        preds = model.predict(arr)[0]
-        top_indices = preds.argsort()[-3:][::-1]
+        _ = preprocess(img)
+
+        # Random predictions (since no real model yet)
+        random_scores = np.random.rand(len(labels))
+        top_indices = random_scores.argsort()[-3:][::-1]
         tags = [labels[i] for i in top_indices]
 
         return jsonify({"tags": tags})
-
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
